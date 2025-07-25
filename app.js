a937f7274487e78eb2ac2515c4f6bc9c8141360a
dblimma06 <152748715+dblimma06@users.noreply.github.com>
2025-07-24 22:39:10 -0300

    adicionando os arquivos por upload

---
+++ app.js
@@ -0,0 +1,33 @@
+
+alert('Boas vindas ao jogo do número secreto');
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        break
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        //tentativas = tentativas += 1;
+        tentativas ++;
+        console.log(tentativas)
+    }
+}
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Você Acertou!!! O número era ${numeroSecreto} e você acertou com ${tentativas} ${palavraTentativa} :D`)
+
+//if (tentativas > 1) {
+//alert(`Você Acertou!!! O número era ${numeroSecreto} e você acertou com ${tentativas} tentativas :D`)
+//} else {
+//    alert(`Você Acertou!!! O número era ${numeroSecreto} e você acertou com ${tentativas} tentativa :D`)
+// }
